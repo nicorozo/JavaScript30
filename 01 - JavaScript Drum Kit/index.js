6394e5ca559313eca529f7a9a6200d327f8f743a
Nico Rozo <nicolas.arenas.rozo@gmail.com>
2023-08-11 17:54:03 +0200

    01 completed

--- 01 - JavaScript Drum Kit/index.js
+++ 01 - JavaScript Drum Kit/index.js
@@ -1,13 +1,21 @@
 window.addEventListener('keydown', function (e) {
 
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = this.document.querySelector(`div[data-key="${e.keyCode}"]`)
+    const audio = document.querySelector(`audio[data-key="${e.code}"]`);
+    const key = document.querySelector(`div[data-key="${e.code}"]`)
     console.log(audio) // "e" stands for event
     if (!audio) return;
 
     audio.currentTime = 0;
     audio.play();
 
-    if (key)
-        key.classList.toggle('playing');
+    key.classList.add('playing');
 })
+function removeTransition(e) {
+    console.log(e)
+    if (e.propertyName !== 'transform') return
+    this.classList.remove('playing')
+
+}
+
+const keys = document.querySelectorAll('.key')
+keys.forEach(key => key.addEventListener('transitionend', removeTransition))
